25110b18245990cd527876548519cb0be6f5e95f
chinobv <retrochanel97@gmail.com>
2020-03-17 04:21:19 -0600

    Use un modal para mostrar los emails en pedidos

--- client/src/components/pedidos/DatosCliente.jsx
+++ client/src/components/pedidos/DatosCliente.jsx
@@ -15,29 +15,48 @@ function DatosCliente({ id }) {
 						const { nombre, apellido, edad, emails, empresa, tipo } = data.getCliente;
 						
 						return(
-							<ul className="list-unstyled my-5">
-								<li className="border font-weight-bold p-2">
-									Nombre: 
+							<ul className="list-unstyled my-5 list-group">
+								<li className="border font-weight-bold p-2 list-group-item">Nombre: 
 									<span className="font-weight-normal">{nombre}</span>
 								</li>
-								<li className="border font-weight-bold p-2"> 
-									Apellido: 
+								<li className="border font-weight-bold p-2 list-group-item"> Apellido: 
 									<span className="font-weight-normal">{apellido}</span>
 								</li>
-								<li className="border font-weight-bold p-2"> 
-									Edad: 
+								<li className="border font-weight-bold p-2 list-group-item"> Edad: 
 									<span className="font-weight-normal">{edad}</span>
 								</li>
-								<li className="border font-weight-bold p-2"> 
-									Emails: 
-									<span className="font-weight-normal">{emails.map(email => `${email.email}`)}</span>
+								<li className="border font-weight-bold p-2 list-group-item">
+									<button 
+										type="button" 
+										className="btn btn-primary d-block d-md-inline-block mr-2 text-center" 
+										data-toggle="modal" 
+										data-target="#VerEmails">
+										Ver Emails
+									</button>
+
+									<div className="modal fade" id="VerEmails" tabindex="-1" role="dialog" aria-labelledby="exampleModalScrollableTitle" aria-hidden="true">
+									  <div className="modal-dialog modal-dialog-scrollable" role="document">
+									    <div className="modal-content">
+									      <div className="modal-header">
+									        <h5 className="modal-title" id="exampleModalScrollableTitle">Emails de: {nombre}</h5>
+									        <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+									          <span aria-hidden="true">&times;</span>
+									        </button>
+									      </div>
+									      <div className="modal-body">
+									        {emails.map(email => <h5>{email.email}</h5>)}
+									      </div>
+									      <div className="modal-footer">
+									        <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+									      </div>
+									    </div>
+									  </div>
+									</div>
 								</li>
-								<li className="border font-weight-bold p-2"> 
-									Empresa: 
+								<li className="border font-weight-bold p-2 list-group-item"> Empresa: 
 									<span className="font-weight-normal">{empresa}</span>
 								</li>
-								<li className="border font-weight-bold p-2"> 
-									tipo: 
+								<li className="border font-weight-bold p-2 list-group-item"> tipo: 
 									<span className="font-weight-normal">{tipo}</span>
 								</li>
 							</ul>
