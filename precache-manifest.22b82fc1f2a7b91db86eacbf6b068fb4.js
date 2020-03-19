1c98f7453db64cea034fa32489e301d1d64cc5f4
chinobv <morenoresendez03>
2020-03-18 21:12:30 -0600

    Updates

---
+++ precache-manifest.22b82fc1f2a7b91db86eacbf6b068fb4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd9f02e8efe5d8080d17d041a066fb56",
+    "url": "/app-fulstack/index.html"
+  },
+  {
+    "revision": "3d5f8a8ee2cdba2e4917",
+    "url": "/app-fulstack/static/css/main.c6c1de92.chunk.css"
+  },
+  {
+    "revision": "2ce2d2e49e733611eb86",
+    "url": "/app-fulstack/static/js/2.d105286c.chunk.js"
+  },
+  {
+    "revision": "daf427c65b4cb8e13289c1e6834d5d64",
+    "url": "/app-fulstack/static/js/2.d105286c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d5f8a8ee2cdba2e4917",
+    "url": "/app-fulstack/static/js/main.83255267.chunk.js"
+  },
+  {
+    "revision": "ef18cf309f3e3a8e61be",
+    "url": "/app-fulstack/static/js/runtime-main.1c7f95e9.js"
+  }
+]);
