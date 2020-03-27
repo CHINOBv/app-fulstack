51a3f13a97747f66149610d92f70d7001513a650
chinobv <retrochanel97@gmail.com>
2020-03-26 21:38:56 -0600

    Cree las graficas y use el query TopClientes para mostrar los datos en la grafica

---
+++ client/src/components/panel/Clients.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Query } from 'react-apollo';
+import { TOP_CLIENTES } from '../../querys'
+
+import Spiner from '../layaut/Spiner.jsx';
+import {BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend} from "recharts";
+
+const Clients = () => {
+	return (
+		<Query query={ TOP_CLIENTES } pollInterval={500}>
+			{({ loading, error, data, startPolling, stopPolling }) =>{
+				if(loading) return <Spiner/>;
+				if(error) return `Error: ${ error.message }`
+				const topClientesGraf = [];
+				data.topClientes.map(( pedido,index ) => {
+					topClientesGraf[index] = {
+						...pedido.cliente[0],
+						total: pedido.total
+					}
+				})
+				console.log(topClientesGraf)
+				return (
+			    	<BarChart width={600} height={300} data={topClientesGraf}
+			            margin={{top: 5, right: 30, left: 20, bottom: 5}}>
+				       <CartesianGrid strokeDasharray="3 3"/>
+				       <XAxis dataKey="nombre"/>
+				       <YAxis/>
+				       <Tooltip/>
+				       <Bar dataKey="total" fill="#82ca9d" />
+			      </BarChart>
+				)
+			}}
+		</Query>
+	)
+}
+
+export default Clients
--- client/src/components/panel/Panel.jsx
+++ client/src/components/panel/Panel.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Clients from './Clients.jsx'
 
 const Panel = () => {
 	return (
 		<div>
 			<h1>Panel</h1>
+			<Clients/>
 		</div>
 	)
 }
